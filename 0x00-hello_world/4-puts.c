8208c857c5d91e05e1afb33f3078010a27ff3b78
Demitsi <demitritsinonis@gmail.com>
2023-02-16 13:21:35 -0800

    4-Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *main - Write a C program that prints what is needed followed by a new line 
+ *main- Write a c program that prints what is needed followed by a new line 
  *Return: 0(success)
  */
-    int main(void)
+    intmain(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
