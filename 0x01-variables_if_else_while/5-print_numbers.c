55715d69fc33f11273bb311987401b2e46f4180b
Demitsi <demitritsinonis@gmail.com>
2023-02-17 15:46:10 -0800

    5-Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+i#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: a program that prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+		printf("\n");
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,1 @@
-a program which prints the alphabet in lowercase, followed by a new line.Print all the letters except q and e
+a program which prints all single digit numbers of base 10 starting from 0, followed by a new line
