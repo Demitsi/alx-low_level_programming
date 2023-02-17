4faf64597c31a5631028d561f7f1028b167ab4eb
Demitsi <demitritsinonis@gmail.com>
2023-02-17 11:41:33 -0800

    2-I sometimes suffer from insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-/**
- * main - Entry point
- * Description: 'a program which prints the alphabet in lowercase, followed by a new line'
- *
- * Return: 0 (success)
- */
+
 int main(void)
 {
-	char low;
+    //declare a variable
+    char alphabet;
 
-	/* a program that prints the alphabet in lowercase, followed by a new line*/
+    for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+    {
+        // print the value of the variable
+        putchar(alphabet);
 
-	for(low = 'a'; low <= 'z'; low++)
-		putchar(low);
-		putchar('\n');
-	
-	return (0);
+    }
+
+    putchar('\n');
+    return (0);
 }
+
