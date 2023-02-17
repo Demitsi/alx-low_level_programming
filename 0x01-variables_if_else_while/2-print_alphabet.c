8393d9a2812badddf9e9b6c839b1d0aedc2e1b5b
Demitsi <demitritsinonis@gmail.com>
2023-02-17 12:33:46 -0800

    2-I sometimes suffer from insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,13 +8,13 @@
  * Return: 0 (success)
  */
 int main(void)
-	
-char alphabet;
+{	
+	char alphabet;
 
-for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
-{
-       	putchar(alphabet);
-}
-putchar('\n');
-return (0);
+	for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+	{
+		putchar(alphabet);
+	}	
+		putchar('\n');
+	return (0);
 }
