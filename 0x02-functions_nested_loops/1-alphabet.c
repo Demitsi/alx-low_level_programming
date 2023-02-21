05a7f3be536e6cff0f1f8ba8b58c9b9aae3b993f
Demitsi <demitritsinonis@gmail.com>
2023-02-21 08:43:49 -0800

    1-I play what i call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,1 @@
-#include "main.h"
-/**
- * main - Print the alphabet in lowercase
- *Description: 'A function which prints the alphabet in lowercase, followed by a new line'
- *
- * Return: 0 (success)
- */
-int main(void)
-{
-    print_alphabet(alphabet = 'a-z'; alphabet < z; a++);
-
-    return (0);
-}
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 1-main.c 1-alphabet.c -o 1-alphabet
