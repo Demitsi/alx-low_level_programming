31236cc8e1523d63b6d1ec79b25f14c1b4a1622e
Demitsi <demitritsinonis@gmail.com>
2023-02-21 12:06:37 -0800

    2-10x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,23 +2,30 @@
 /**
 * print_alphabet_x10 - It is to print 10 times the alphabet in lowercase
 *
+* Return: 0 (success)
 */
 void print_alphabet_x10(void)
 {
-char ch;
+char alphabet;
 int i;
 ((i) = 0);
 
 while (i < 10)
 {
-	ch = 'a';
-}
-while (ch <= 'z')
-{
-	_putchar(ch);
+	(char = 'a');
 }
 {
-	ch++;
+	(char < 'z');
+}
+{
+	(char++);
+}
+while (char <= 'z')
+{
+	_putchar(char);
+}
+{
+	char++;
 }
 {
 	_putchar('\n');
@@ -26,4 +33,5 @@ while (ch <= 'z')
 {
 	i++;
 }
+return (0);
 }
