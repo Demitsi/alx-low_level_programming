2c878d4eef604a683913c445c87d77d1c5282bc8
Demitsi <demitritsinonis@gmail.com>
2023-02-27 04:54:37 -0800

    0-98 Battery st

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - It is a function which writes the character c to stdout
+ * @c : It informs the character to print
+ *
+ *
+ * Return: On 1 (success)
+ * On an error, -1 returned, errno is set appropriatly
+ */
+int _putchar(char c)
+{
+	retrun(write(1, &x, 1));
+}
