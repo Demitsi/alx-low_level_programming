50da359a9ad335c7f8bd288629a240b4f136688a
Demitsi <demitritsinonis@gmail.com>
2023-03-06 06:23:41 -0800

    4-strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,7 +4,7 @@
  * _strpbrk -  Is a function which searches a string for any of a set of bytes.
  * @s: Is an input string
  * @accept: Is an input character with which to locate into string s
- * Return: Is a pointer to the byte in s which matches one of the bytes in accept,
+ * Return: A pointer to the byte in s which matches one of the bytes in accept,
  * or NULL if no such byte is found
  */
 char *_strpbrk(char *s, char *accept)
