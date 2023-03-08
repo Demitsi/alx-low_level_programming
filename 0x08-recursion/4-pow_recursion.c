2d791eddfbab07712569170a87b849076228b66e
Demitsi <demitritsinonis@gmail.com>
2023-03-08 04:47:46 -0800

    4-Once an idea has taken hold

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,25 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * _pow_recursion - It returns the value of x raised to the power y.
+ * @x: Is the base
+ * @y: Is the exponent
+ * Return: The result of x raised y
  */
-int main(void)
+int _pow_recursion(int x, int y)
 {
-	int r;
-
-	r = _pow_recursion(1, 10);
-	printf("%d\n", r);
-	r = _pow_recursion(1024, 0);
-	printf("%d\n", r);
-	r = _pow_recursion(2, 16);
-	printf("%d\n", r);
-	r = _pow_recursion(5, 2);
-	printf("%d\n", r);
-	r = _pow_recursion(5, -2);
-	printf("%d\n", r);
-	r = _pow_recursion(-5, 3);
-	printf("%d\n", r);
-	return (0);
+	if (y == 0)
+		return (1);
+	else if (y < 0)
+		return (-1);
+	else if (y == 1)
+		return (x);
+	return (x *= _pow_recursion(x, y - 1));
 }
