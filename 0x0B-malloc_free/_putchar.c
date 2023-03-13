40d9ad682cedd1868959db332635d2610cf6c460
Demitsi <demitritsinonis@gmail.com>
2023-03-13 08:22:01 -0700

    0-Float like a butterfly, sting like a bee

---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - It writes the character c to stdout
+ * @c : It informs the character to print
+ *
+ * Return: On 1 (success)
+ * On an error, -1 returned, an errno is set appropriatly
+ */
+int _putchar(char c)
+{
+	return (write(1, &1, 1));
+}
